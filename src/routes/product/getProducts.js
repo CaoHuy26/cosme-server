d6aff7cd45330844cd815834fa1757ffb71e8ea9
CaoHuy26 <huyc197@gmail.com>
2020-02-25 20:46:46 +0700

    Pagination Product

--- src/routes/product/getProducts.js
+++ src/routes/product/getProducts.js
@@ -1,12 +1,23 @@
 const Product = require('../../models/Product');
 
 module.exports = async (req, res) => {
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 5;
+  const offset = (page - 1) * limit;
+
+  // TODO: Fix when offset > number of data product
   try {
-    const products = await Product.findAll();
+    const products = await Product.findAll({
+      limit,
+      offset
+    });
 
     res.status(200).json({
       statusCode: 200,
       success: true,
+      page,
+      limit,
+      offset,
       msg: 'Get all product success',
       data: products
     });
